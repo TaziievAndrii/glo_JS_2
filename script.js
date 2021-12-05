1505f6daca9b217f2ee55ab071b219b8c6ab86b7
Андрей <andrew13369@icloud.com>
2021-12-05 10:08:06 +0200

    исправил замыкание

--- script.js
+++ script.js
@@ -1,31 +1,29 @@
 'use strict';
 
 const getRandomNumber = function(min, max) {
-    return Math.floor(Math.random() * (max-min) + min)
+    let num = Math.floor(Math.random() * (max-min) + min)
+
+    function searchNumb() {
+    
+        const ask = prompt('Угадай число от 1 до 100')
+    
+        if (ask === null) return alert("Игра окончена")
+    
+        if(num < ask ) {
+        alert("Загаданное число меньше") 
+        searchNumb() 
+        } else if (num > ask){ 
+            alert("Загаданное число больше")
+            searchNumb() 
+        } else if(isNaN(ask)) {
+            alert('Введи число!')
+            searchNumb() 
+        } else if ( ask == num) {
+            alert('"Поздравляю, Вы угадали!!!"')
+            return
+        } 
+    }
+    return searchNumb ()
 }
 
-let num = getRandomNumber(1, 100);
-
-function searchNumb() {
-    let numb = num
-
-    const ask = prompt('Угадай число от 1 до 100')
-
-    if (ask === null) return alert("Игра окончена")
-
-    if(numb < ask ) {
-    alert("Загаданное число меньше") 
-    searchNumb() 
-    } else if (numb > ask){ 
-        alert("Загаданное число больше")
-        searchNumb() 
-    } else if(isNaN(ask)) {
-        alert('Введи число!')
-        searchNumb() 
-    } else if ( ask == numb) {
-        alert('"Поздравляю, Вы угадали!!!"')
-        return
-    } 
-}
-
-searchNumb()
+getRandomNumber(1, 100);

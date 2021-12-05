5eefbebd7305ec8f2d8348755706c064eaf4f486
Андрей <andrew13369@icloud.com>
2021-12-05 11:06:57 +0200

    сделал изменения

--- script.js
+++ script.js
@@ -1,25 +1,28 @@
 'use strict';
 
 const getRandomNumber = function(min, max) {
+    
     let num = Math.floor(Math.random() * (max-min) + min)
 
     function searchNumb() {
-    
+
+        let numb = num
+        
         const ask = prompt('Угадай число от 1 до 100')
     
         if (ask === null) return alert("Игра окончена")
     
-        if(num < ask ) {
+        if(numb < ask ) {
         alert("Загаданное число меньше") 
         searchNumb() 
-        } else if (num > ask){ 
+        } else if (numb > ask){ 
             alert("Загаданное число больше")
             searchNumb() 
         } else if(isNaN(ask)) {
             alert('Введи число!')
             searchNumb() 
-        } else if ( ask == num) {
-            alert('"Поздравляю, Вы угадали!!!"')
+        } else if ( +ask === numb) {
+            alert("Поздравляю, Вы угадали!!!")
             return
         } 
     }
